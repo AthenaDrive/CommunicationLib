d119844e21c1bc73b72a0ebd1b837406d0f9bc15
IslandRock1 <oysteinmb@gmail.com>
2026-07-02 02:24:57 +0200

    Implement functions for generating TCP streams

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,120 @@
 
 #include "Ethernet.hpp"
+#include "Protocol.hpp"
+#include "RuntimeError.hpp"
+
+std::vector<uint8_t> getTcpCommandPacket(const std::vector<std::pair<std::vector<uint32_t>, std::vector<uint32_t>>>& values) {
+	std::vector<uint32_t> headers;
+	std::vector<std::vector<uint32_t>> packetVals;
+	uint32_t numValues = 0;
+
+	for (int i = 0; i < values.size(); i++) {
+		if ((values[i].first[0] != TCP_DATA) &&
+			(values[i].first[0] != TCP_COMMAND)) {
+			throw RuntimeError("The first header bit MUST be either DATA or COMMAND.\n");
+		}
+
+		for (int v = 1; v < values[i].first.size(); v++) {
+			if ((values[i].first[v] == TCP_DATA) ||
+				(values[i].first[v] == TCP_COMMAND)) {
+				throw RuntimeError("Only the first header bit can be DATA or COMMAND.\n");
+			}
+		}
+
+		if (values[i].first.size() - 1 != values[i].second.size()) {
+			// Horrible error message.. idk.
+			// This function expects exactly 1 (the first)
+			// header value to be either the DATA or COMMAND flag.
+
+			// The rest of the header values should match up with each of the values.
+			throw RuntimeError("Num values must equal active header values.\n");
+		}
+
+		headers.emplace_back(0);
+		packetVals.emplace_back();
+
+		for (auto &v : values[i].first) {
+			headers[i] |= v;
+		}
+
+		for (auto &v : values[i].second) {
+			packetVals[i].emplace_back(v);
+			numValues++;
+		}
+	}
+
+	std::vector<uint8_t> outVec{};
+	for (int i = 0; i < headers.size(); i++) {
+		std::cout << "Header size: " << headers.size() << " | i: " << i << " | NumVal: " << numValues << "\n";
+		uint32_t prefix = PREFIX(((2 * (headers.size() - i) - 1) + numValues) * 4);
+		outVec.push_back((prefix >> 0) & 0xFF);
+		outVec.push_back((prefix >> 8) & 0xFF);
+		outVec.push_back((prefix >> 16) & 0xFF);
+		outVec.push_back((prefix >> 24) & 0xFF);
+
+		outVec.push_back((headers[i] >> 0) & 0xFF);
+		outVec.push_back((headers[i] >> 8) & 0xFF);
+		outVec.push_back((headers[i] >> 16) & 0xFF);
+		outVec.push_back((headers[i] >> 24) & 0xFF);
+
+		std::cout << "Prefix: " << prefix << " | Header: " << headers[i] << "\n";
+		for (auto &v : packetVals[i]) {
+			outVec.push_back((v >> 0) & 0xFF);
+			outVec.push_back((v >> 8) & 0xFF);
+			outVec.push_back((v >> 16) & 0xFF);
+			outVec.push_back((v >> 24) & 0xFF);
+			numValues--;
+
+			std::cout << "V: " << v << "\n";
+		}
+	}
+
+	return outVec;
+}
+
+std::vector<uint8_t> getTcpDataPacket(const std::vector<std::vector<uint32_t>>& values) {
+	std::vector<uint32_t> headers;
+
+	for (int i = 0; i < values.size(); i++) {
+		if ((values[i][0] != TCP_DATA) && (values[i][0] != TCP_COMMAND)) {
+			throw RuntimeError("The first header bit MUST be either DATA or COMMAND.\n");
+		}
+
+		for (int v = 1; v < values[i].size(); v++) {
+			if ((values[i][v] == TCP_DATA) ||
+				(values[i][v] == TCP_COMMAND)) {
+				throw RuntimeError("Only the first header bit can be DATA or COMMAND.\n");
+			}
+		}
+
+		headers.emplace_back(0);
+		for (auto &v : values[i]) {
+			headers[i] |= v;
+		}
+	}
+
+	std::vector<uint8_t> outVec{};
+	for (int i = 0; i < headers.size(); i++) {
+		uint32_t prefix = PREFIX((2 * (headers.size() - i) - 1) * 4);
+		outVec.push_back((prefix >> 0) & 0xFF);
+		outVec.push_back((prefix >> 8) & 0xFF);
+		outVec.push_back((prefix >> 16) & 0xFF);
+		outVec.push_back((prefix >> 24) & 0xFF);
+
+		outVec.push_back((headers[i] >> 0) & 0xFF);
+		outVec.push_back((headers[i] >> 8) & 0xFF);
+		outVec.push_back((headers[i] >> 16) & 0xFF);
+		outVec.push_back((headers[i] >> 24) & 0xFF);
+
+		std::cout << "Prefix: " << prefix << " | Header: " << headers[i] << "\n";
+	}
+
+	return outVec;
+}
 
 int main() {
 
 	Ethernet eth;
-
-	uint32_t lengthPrefixReadVoltCurr = 0b11111000011110010000000000000100;
-
-	// Firs bit 0 => Read data (Not command)
-	// Second bit 1 => Bus voltage
-	// Third bit 1 => Bus current
-	uint32_t headerReadVoltCurr = 0b00000000000000000000000000000110;
-
-	uint32_t lengthPrefixSetUDPHeader = 0b11111000011110010000000000001000;
-	uint32_t headerSetUDPHeader = (1 << 0) | (1 << 19);
-	uint32_t packetSetUDPHeader = 0b0000000000000000000000000001101;
-
-	uint32_t lengthPrefixDual = 0b11111000011110010000000000010100;
 
 	while (true) {
 		auto udpDataArr = eth.getUDPData();
@@ -28,41 +126,22 @@ int main() {
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-		uint32_t lengthPrefix = lengthPrefixDual;
-		uint32_t header = headerSetUDPHeader;
-		uint32_t packet = packetSetUDPHeader;
+		// auto outVec = getTcpDataPacket({
+		// 	{TCP_DATA, TCP_DATA_BUS_CURRENT, TCP_DATA_BUS_VOLTAGE},
+		// 	{TCP_DATA, TCP_DATA_BUS_CURRENT, TCP_DATA_BUS_VOLTAGE},
+		// 	{TCP_DATA, TCP_DATA_BUS_CURRENT, TCP_DATA_BUS_VOLTAGE}
+		// });
 
-		std::vector<uint8_t> outVec{};
-		outVec.push_back((lengthPrefix >> 0) & 0xFF);
-		outVec.push_back((lengthPrefix >> 8) & 0xFF);
-		outVec.push_back((lengthPrefix >> 16) & 0xFF);
-		outVec.push_back((lengthPrefix >> 24) & 0xFF);
+		auto outVec = getTcpCommandPacket({
+			{{TCP_COMMAND, TCP_COMMAND_TORQUE_SETPOINT}, {1010101}},
+			{{TCP_COMMAND, TCP_COMMAND_TORQUE_SETPOINT, TCP_COMMAND_CLEAR_ERROR_REGISTER}, {101, 101, 1}}
+		});
 
-		outVec.push_back((header >> 0) & 0xFF);
-		outVec.push_back((header >> 8) & 0xFF);
-		outVec.push_back((header >> 16) & 0xFF);
-		outVec.push_back((header >> 24) & 0xFF);
-
-		outVec.push_back((packet >> 0) & 0xFF);
-		outVec.push_back((packet >> 8) & 0xFF);
-		outVec.push_back((packet >> 16) & 0xFF);
-		outVec.push_back((packet >> 24) & 0xFF);
-
-		lengthPrefix = lengthPrefixSetUDPHeader;
-		outVec.push_back((lengthPrefix >> 0) & 0xFF);
-		outVec.push_back((lengthPrefix >> 8) & 0xFF);
-		outVec.push_back((lengthPrefix >> 16) & 0xFF);
-		outVec.push_back((lengthPrefix >> 24) & 0xFF);
-
-		outVec.push_back((header >> 0) & 0xFF);
-		outVec.push_back((header >> 8) & 0xFF);
-		outVec.push_back((header >> 16) & 0xFF);
-		outVec.push_back((header >> 24) & 0xFF);
-
-		outVec.push_back((packet >> 0) & 0xFF);
-		outVec.push_back((packet >> 8) & 0xFF);
-		outVec.push_back((packet >> 16) & 0xFF);
-		outVec.push_back((packet >> 24) & 0xFF);
+		std::cout << "Packet: ";
+		for (auto &v : outVec) {
+			std::cout << +v;
+		}
+		std::cout << "\n";
 
 		eth.setTCPSend(outVec);
 
@@ -71,10 +150,11 @@ int main() {
 
 			std::vector<uint8_t> recvBuffer{};
 			eth.getTCPRecv(recvBuffer);
-			// std::cout << "Count: " << eth.numTCPReads.load() << " | ";
+			std::cout << "Count: " << eth.numTCPReads.load() << " | ";
 			for (uint8_t val : recvBuffer) {
-				// std::cout << static_cast<int>(val) << " ";
+				std::cout << static_cast<int>(val) << " ";
 			}
+			std::cout << "\n";
 		}
 	}
 }
